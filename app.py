d626986ddff6aca4acfd48a067ea0118fadabbbb
gbk1414 <gbk1414@gmail.com>
2024-01-31 16:43:23 +0900

    웹툰 페이지를 열 때 db 내용 초기화 후 api에서 받은 값으로 db에 입력

--- app.py
+++ app.py
@@ -1,7 +1,69 @@
 from flask import Flask, render_template
+import os
+from flask_sqlalchemy import SQLAlchemy
 import requests
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'database.db')
+
+db = SQLAlchemy(app)
+
+class Webtoon(db.Model):
+    titleId = db.Column(db.Integer, primary_key=True, nullable=False)
+    titleName = db.Column(db.String, nullable=True)
+    author = db.Column(db.String, nullable=True)
+    thumbnail_url = db.Column(db.String, nullable=True)
+    up = db.Column(db.String, nullable=True)
+    rest = db.Column(db.String, nullable=True)
+    bm = db.Column(db.String, nullable=True)
+    adult = db.Column(db.String, nullable=True)
+    starScore = db.Column(db.Float, nullable=True)
+    viewCount = db.Column(db.String, nullable=True)
+    openToday = db.Column(db.String, nullable=True)
+    potenUp = db.Column(db.String, nullable=True)
+    bestChallengeLevelUp = db.Column(db.String, nullable=True)
+    finish = db.Column(db.String, nullable=True)
+    new = db.Column(db.String, nullable=True)
+
+    def __repr__(self):
+        return f"<Webtoon {self.titleName}>"
+
+with app.app_context():
+    db.create_all()
+
+
+def update_webtoon_data():
+    r = requests.get(
+        'https://comic.naver.com/api/webtoon/titlelist/weekday?week=dailyPlus&order=user%27')
+    rjson = r.json()
+    webtoon_data = rjson['titleList']
+
+    # 데이터베이스 초기화
+    db.session.query(Webtoon).delete()
+
+    for webtoon_info in webtoon_data:
+        webtoon = Webtoon(
+            titleId=webtoon_info['titleId'],
+            titleName=webtoon_info['titleName'],
+            author=webtoon_info['author'],
+            up=str(webtoon_info['up']),
+            thumbnail_url=webtoon_info['thumbnailUrl'],
+            rest=str(webtoon_info['rest']),
+            bm=str(webtoon_info['bm']),
+            adult=str(webtoon_info['adult']),
+            starScore=webtoon_info['starScore'],
+            viewCount=str(webtoon_info['viewCount']),
+            openToday=str(webtoon_info['openToday']),
+            potenUp=str(webtoon_info['potenUp']),
+            bestChallengeLevelUp=str(webtoon_info['bestChallengeLevelUp']),
+            finish=str(webtoon_info['finish']),
+            new=str(webtoon_info['new'])
+        )
+        db.session.add(webtoon)
+
+    db.session.commit()
 
 @app.route("/")
 def home():
@@ -14,16 +76,20 @@ def home():
     }
     return render_template('motto.html', data=context)
 
+
 @app.route('/webtoon')
 def webtoon():
-    
-    r = requests.get('https://comic.naver.com/api/webtoon/titlelist/weekday?week=dailyPlus&order=user%27')
+
+    r = requests.get(
+        'https://comic.naver.com/api/webtoon/titlelist/weekday?week=dailyPlus&order=user%27')
     rjson = r.json()
 
     webtoon_data = rjson['titleList']
 
+    update_webtoon_data()
+
     return render_template('webtoon.html', data=webtoon_data)
+
 
 if __name__ == '__main__':
     app.run()
-
